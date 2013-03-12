d170e48738d9ac1a3d01e9b186d839e77aba2a6e
David Dvorak <david.dvorak@webtrends.com>
2013-03-12 20:49:40 +0000

    change attributes to default; add version 5.1

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: vmware-tools
-# attributes::default.rb
+# Attributes:: default
 #
 # Copyright 2011, Bryan W. Berry <bryan.berry@gmail.com>
 #
@@ -14,17 +14,21 @@ esx_hw_id = %x[ dmidecode | grep -A4 "BIOS Information" | grep 'Address:' ].spli
 
 case esx_hw_id
 when "0xEA550"
-  set['esx']['version'] = "4.0"
-  set['esx']['tarball'] = "VMwareTools-4.0.0-208167.tar.gz"
-  set['esx']['checksum'] = "f6f0d2fce35d2abdecf97a522c0b686f709758d37573a4b5dff7a86a17cc485a"
+  default['esx']['version'] = "4.0"
+  default['esx']['tarball'] = "VMwareTools-4.0.0-208167.tar.gz"
+  default['esx']['checksum'] = "f6f0d2fce35d2abdecf97a522c0b686f709758d37573a4b5dff7a86a17cc485a"
 when "0xEA2E0"
-  set['esx']['version'] = "4.1"
-  set['esx']['tarball'] = "VMwareTools-8.3.7-341836.tar.gz"
-  set['esx']['checksum'] = "112092df38d3e82e41bbeda2cf0e97a6ccfae845d6e8dd0d058d635aa994365f"
-when "0xE72C0" 
-  set['esx']['version'] = "5.0"
-  set['esx']['tarball'] = "VMwareTools-8.6.0-446312.tar.gz"
-  set['esx']['checksum'] = "5118df75830e416ca20e2939e4934b4ebb8b9e29160e3fbed225aaa832345712"
+  default['esx']['version'] = "4.1"
+  default['esx']['tarball'] = "VMwareTools-8.3.7-341836.tar.gz"
+  default['esx']['checksum'] = "112092df38d3e82e41bbeda2cf0e97a6ccfae845d6e8dd0d058d635aa994365f"
+when "0xE72C0"
+  default['esx']['version'] = "5.0"
+  default['esx']['tarball'] = "VMwareTools-8.6.0-446312.tar.gz"
+  default['esx']['checksum'] = "5118df75830e416ca20e2939e4934b4ebb8b9e29160e3fbed225aaa832345712"
+when "0xEA0C0"
+  default['esx']['version'] = "5.1"
+  default['esx']['tarball'] = "VMwareTools-9.0.1-913578.tar.gz"
+  default['esx']['checksum'] = "e3c116eaa0693774d1ced90fefea7f65de246b7914a9218df6bb4549c0ae4c81"
 end
 
 Chef::Log.debug("esx tarball is #{node['esx']['tarball']}")
