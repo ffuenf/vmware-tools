00f0a5fdeeb90f20363ee33560c6e820f204073a
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2014-08-19 18:18:59 +0200

    add name attribute to make Berkshelf happy;

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,11 @@
-maintainer       "Achim Rosenhagen"
-maintainer_email "a.rosenhagen@ffuenf.de"
-license          "Apache 2.0"
-description      "installs/configures vmware-tools"
+maintainer 'Achim Rosenhagen'
+maintainer_email 'a.rosenhagen@ffuenf.de'
+license 'Apache 2.0'
+description 'installs/configures vmware-tools'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+name 'vmware-tools'
+version '1.0.0'
 
-%w{ debian }.each do |os|
+%w(debian).each do |os|
   supports os
 end
